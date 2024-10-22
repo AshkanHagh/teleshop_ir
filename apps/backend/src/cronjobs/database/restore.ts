28fbcff5d2a6eedfd2bd437abcd6ade265c2dbf6
Ashkan <AshkanHagh01@gmail.com>
2024-10-22 07:41:44 +0330

    feat(postgres): implement backup and restore functionality and add profile to docker-compose

---
+++ apps/backend/src/cronjobs/database/restore.ts
@@ -0,0 +1,20 @@
+import { $, Glob } from 'bun';
+import { env } from '../../../env';
+
+const restorePgBackup = async () => {
+    console.log('Restoring backup process started');
+
+    const glob : Glob = new Glob('*.sql');
+    const filesName : Set<string> = new Set();
+    for (const file of glob.scanSync(`${__dirname}/../database/backups/`)) {
+        const backupFineName : string = file.split('.')[0];
+        filesName.add(backupFineName);
+    }
+    const newestBackupFileName : string = Array.from(filesName.values()).sort((a, b) => +b - +a)[0];
+    const sqlFilePath : string = `/app/src/database/backups/${newestBackupFileName}.sql`;
+    await $`PGPASSWORD=${env.POSTGRES_PASSWORD} psql -U ${env.POSTGRES_USER} -h postgres -d ${env.POSTGRES_DB} -f ${sqlFilePath}`;
+
+    console.log('Restoring backup process ended successfully');
+}
+
+restorePgBackup();
