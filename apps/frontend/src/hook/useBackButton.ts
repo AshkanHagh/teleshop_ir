01827262cdd0917e80c764aaba941d75ea8e4432
ShahinFallah <shahinfallah2006@gmail.com>
2024-11-27 09:21:11 +0330

    fix: fix useBackButton bug

--- apps/frontend/src/hook/useBackButton.ts
+++ apps/frontend/src/hook/useBackButton.ts
@@ -7,20 +7,29 @@ const useBackButton = () => {
     const tg = Telegram.WebApp
 
     useEffect(() => {
+        location.pathname !== '/' && navigate('/')
+    }, [])
+
+
+    useEffect(() => {
+        const handleBackButtonClick = () => {
+            location.key === 'default' ? navigate('/') : navigate(-1)
+        }
+
         if (location.pathname === '/') {
             tg.BackButton.hide()
             return
         }
-        
+
         tg.BackButton.show()
-        tg.BackButton.onClick(() => {
-            navigate(-1)
-        })
+        tg.BackButton.offClick(handleBackButtonClick)
+        tg.BackButton.onClick(handleBackButtonClick)
 
         return () => {
             tg.BackButton.hide()
+            tg.BackButton.offClick(handleBackButtonClick)
         }
-    }, [location.pathname])
+    }, [location.pathname, location.key])
 }
 
 export default useBackButton
