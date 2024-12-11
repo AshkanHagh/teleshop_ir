e9d7b769ba57d7da3f35f9b31583b22a70a9b7f8
ShahinFallah <shahinfallah2006@gmail.com>
2024-12-11 12:33:57 +0330

    chore: remove sentryConfig import from main.tsx

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,12 +1,9 @@
-import { sentryConfig } from '../sentryConfing.ts'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
-
-// sentryConfig()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
