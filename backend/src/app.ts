b79198dc4f3f3b539aeeaee7d0b616b0d24a61db
Ashkan <AshkanHagh01@gmail.com>
2026-08-11 16:29:21 +0330

    fix(build): fix build import paths

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -65,14 +65,17 @@ async function app() {
     await fastify.close();
   });
 
-  await fastify.listen({ port: fastify.config.PORT }, async (error) => {
-    if (error) {
-      console.error(error);
-      process.exit(1);
-    }
-    await fastify.bot.api.setWebhook(fastify.config.TELEGRAM_WEBHOOK_URL, {
-      secret_token: fastify.config.TELEGRAM_WEBHOOK_SECRET,
-    });
-  });
+  await fastify.listen(
+    { port: fastify.config.PORT, host: "0.0.0.0" },
+    async (error) => {
+      if (error) {
+        console.error(error);
+        process.exit(1);
+      }
+      await fastify.bot.api.setWebhook(fastify.config.TELEGRAM_WEBHOOK_URL, {
+        secret_token: fastify.config.TELEGRAM_WEBHOOK_SECRET,
+      });
+    },
+  );
 }
 app();
